eed5d26cdcc08bc76c05f3be1ca6b25a7053d449
Heriyanto <buibui_chen@yahoo.com>
2022-11-05 23:29:25 +1030

    Initial commit

---
+++ app/src/main/java/com/stockwatch/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.stockwatch
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.stockwatch.ui.theme.StockWatchAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StockWatchAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
